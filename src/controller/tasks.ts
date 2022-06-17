0bdeb89ff9d767415fbf5ea78ccdd35ad4f6ddf0
luckyrai1819 <luckyrai1819@gmail.com>
2022-06-17 10:23:08 +0530

    optimized code

--- src/controller/tasks.ts
+++ src/controller/tasks.ts
@@ -4,26 +4,25 @@ const db = require("../db/connect");
 const createTask = async (req: Request, res: Response) => {
   const { name, status } = req.body;
   const sql = `Insert into tasks(name,status) values(?,?)`;
-  const query = db.query(sql, [name, status], (err: Error, result: any) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).json(result);
+  const query = await db.query(
+    sql,
+    [name, status],
+    (err: Error, result: any) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).json(result);
+      }
     }
-  });
+  );
 };
 
 const getAllTasks = async (req: Request, res: Response) => {
-  let { size, start } = req.query;
-  if (!start || Number(start) < 0) {
-    start = "0";
-  }
-  if (!size || Number(size) < 0) {
-    size = "10";
-  }
+  let { size = 10, start = 0 } = req.query;
+
   const sql = `select * from tasks limit ?,?`;
 
-  const query = db.query(
+  const query = await db.query(
     sql,
     [Number(start), Number(size)],
     (err: Error, result: any) => {
@@ -39,22 +38,23 @@ const getAllTasks = async (req: Request, res: Response) => {
 };
 
 const getTask = async (req: Request, res: Response) => {
+  const { id } = req.params;
   const sql = `select * from tasks where id=?`;
-  const query = db.query(sql, req.params.id, (err: Error, result: any) => {
+  const query = await db.query(sql, id, (err: Error, result: any) => {
     if (err) {
       res.status(400).send(err);
     } else if (result.length == 0) {
-      res.status(404).send(`no task with id: ${req.params.id}`);
+      res.status(404).send(`no task with id: ${id}`);
     } else {
       res.status(200).json(result);
     }
   });
 };
 const updateTask = async (req: Request, res: Response) => {
-  const id = req.params.id;
+  const { id } = req.params;
   const { status } = req.body;
   const sql = `update tasks set status=? where id=?`;
-  const query = db.query(sql, [status, id], (err: Error, result: any) => {
+  const query = await db.query(sql, [status, id], (err: Error, result: any) => {
     if (err) {
       res.status(400).send(err);
     } else {
@@ -63,9 +63,9 @@ const updateTask = async (req: Request, res: Response) => {
   });
 };
 const deleteTask = async (req: Request, res: Response) => {
-  const id = req.params.id;
+  const { id } = req.params;
   const sql = `delete from tasks where id=?`;
-  const query = db.query(sql, id, (err: Error, result: any) => {
+  const query = await db.query(sql, id, (err: Error, result: any) => {
     if (err) {
       res.status(400).send(err);
     } else {
