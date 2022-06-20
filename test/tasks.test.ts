3d774ff5892d78cfd2143fab8ece96762657e297
luckyrai1819 <luckyrai1819@gmail.com>
2022-06-20 09:23:25 +0530

    added mocked methods

--- test/tasks.test.ts
+++ test/tasks.test.ts
@@ -2,13 +2,15 @@ import request from "supertest";
 const app = require("../src/app");
 describe("test create task", () => {
   test("correct response", async () => {
-    const res = await request(app).post("/tasks").send({
+    const todo = {
       name: "taskdemo",
-      status: "pending",
-    });
+      status: "completed",
+    };
+
+    const res = await request(app).post("/tasks").send(todo);
     expect(res.body).toEqual({
       name: "taskdemo",
-      status: "pending",
+      status: "completed",
     });
   });
 
@@ -27,7 +29,23 @@ describe("test get all tasks", () => {
       start: 0,
       size: 5,
     });
+    expect(res.getMaxListeners()).toBeLessThanOrEqual(10);
+
+    expect(res.error).toBeFalsy();
+    expect(res.ok).toBeTruthy();
+
     expect(res.body.length).toBe(5);
+    expect(res.body).toContainEqual({
+      id: 5,
+      name: "task2",
+      status: "pending",
+    });
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toEqual(
+      expect.arrayContaining([{ id: 5, name: "task2", status: "pending" }])
+    );
   });
 });
 
@@ -35,12 +53,15 @@ describe("test get single task", () => {
   test("correct response", async () => {
     const res = await request(app).get("/tasks/5");
     expect(res.body).toEqual([{ id: 5, name: "task2", status: "pending" }]);
+    expect(res.body).toStrictEqual([
+      { id: 5, name: "task2", status: "pending" },
+    ]);
   });
 
   test("no task present", async () => {
-    const res = await request(app).get("/tasks/100");
+    const res = await request(app).get("/tasks/1000");
     expect(res.status).toBe(404);
-    expect(res.text).toBe("no task with id: 100");
+    expect(res.text).toBe("no task with id: 1000");
   });
 });
 
@@ -51,13 +72,13 @@ describe("test update task", () => {
     });
     expect(res.status).toBe(200);
     expect(res.body).toBe("task updated");
+    expect(res.body).toMatch(/Updated/i);
   });
 
   test("error response", async () => {
     const res = await request(app).put("/tasks/6").send({
       status: "unknown",
     });
-    console.log(res.body);
     expect(res.status).toBe(400);
   });
 });
@@ -67,5 +88,34 @@ describe("test delete task", () => {
     const res = await request(app).delete("/tasks/8");
     expect(res.status).toBe(200);
     expect(res.body).toBe("Task Deleted");
+    expect(res.body).toBeDefined();
+  });
+});
+
+describe("pracice mocking", () => {
+  test("1", () => {
+    const calculate = (fun: Function, num1: Number, num2: Number) => {
+      return fun(num1, num2);
+    };
+    const Operations = {
+      sum: function (num1: number, num2: number) {
+        return num1 + num2;
+      },
+      multiply: function (num1: number, num2: number) {
+        return num1 * num2;
+      },
+    };
+
+    const mockSum = jest.fn();
+    mockSum.mockImplementation((a, b) => a + b + 1);
+    const mockMultiply = jest.fn();
+    mockMultiply.mockImplementation((a, b) => a * b + 1);
+
+    jest.spyOn(Operations, "sum").mockImplementation(mockSum);
+    jest.spyOn(Operations, "multiply").mockImplementation(mockMultiply);
+
+    // mocked functions will execute instead of real ones.
+    expect(calculate(Operations.sum, 1, 2)).toBe(4);
+    expect(calculate(Operations.multiply, 3, 2)).toBe(7);
   });
 });
